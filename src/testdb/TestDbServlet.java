28ff56ee2ca0126149544fa0864f7ab8d15b0292
lanternavermelha <f_figueira@hotmail.com>
2020-01-02 13:11:45 +0000

    Setting up dev environment

--- src/testdb/TestDbServlet.java
+++ src/testdb/TestDbServlet.java
@@ -38,7 +38,7 @@ public class TestDbServlet extends HttpServlet {
 			
 			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
 			
-			out.println("\nSUCCESS!!!");
+			out.println("SUCCESS!!!");
 			
 			myConn.close();
 		}
